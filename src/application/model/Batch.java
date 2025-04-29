cccbc61070c83a6dde7dc19d5ffc196c002258f8
Magnus Orskov <magnusorskov@hotmail.com>
2025-04-29 12:46:57 +0200

    lavet Batch klasse og tilhørende enums

--- src/application/model/Batch.java
+++ src/application/model/Batch.java
@@ -1,6 +1,47 @@
 package application.model;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class Batch implements Serializable {
+    private Bygsort bygsort;
+    private Mark mark;
+    private String initialer;
+    private String maltBatch;
+    private String rygemateriale;
+    private LocalDate startDato;
+    private LocalDate slutDato;
+    private double alkoholprocent;
+    private double væskemængde;
+    private String kommentar;
+    private int nummer;
+    private static int antalBatches = 1;
+
+    public Batch(Bygsort bygsort, Mark mark, String initialer, String rygemateriale, String maltBatch, LocalDate startDato, String kommentar) {
+        this.bygsort = bygsort;
+        this.mark = mark;
+        this.initialer = initialer;
+        this.rygemateriale = rygemateriale;
+        this.maltBatch = maltBatch;
+        this.startDato = startDato;
+        this.kommentar = kommentar;
+        this.nummer = antalBatches;
+        antalBatches++;
+    }
+
+    public void setSlutDato(LocalDate slutDato) {
+        this.slutDato = slutDato;
+    }
+
+    public void setAlkoholprocent(double alkoholprocent) {
+        this.alkoholprocent = alkoholprocent;
+    }
+
+    public void setVæskemængde(double væskemængde) {
+        this.væskemængde = væskemængde;
+    }
+
+    public void setKommentar(String kommentar) {
+        this.kommentar = kommentar;
+    }
 }
