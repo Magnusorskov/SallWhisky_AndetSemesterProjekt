7242bdb2f33b1f22b6637da6eb8652a12f6bd50a
Magnus Orskov <magnusorskov@hotmail.com>
2025-05-02 13:21:38 +0200

    startet DestillatPane

--- src/gui/DestillatPane.java
+++ src/gui/DestillatPane.java
@@ -1,6 +1,36 @@
 package gui;
 
+import application.controller.Controller;
+import application.model.Batch;
+import javafx.geometry.Insets;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import storage.ListStorage;
 
 public class DestillatPane extends GridPane {
+    private TextArea txaBatchBeskrivelse, txaFadBeskrivelse;
+    private TextField txfNavn, txfAntalLiter;
+    private Label lblTilgængeligLiter;
+
+    public DestillatPane() {
+        this.setPadding(new Insets(20));
+        this.setHgap(20);
+        this.setVgap(10);
+        this.setGridLinesVisible(false);
+
+        Label lblBatches = new Label("Batches");
+        this.add(lblBatches,0,0);
+
+        ComboBox<Batch> cmbBatches = new ComboBox<>();
+        cmbBatches.getItems().addAll(Controller.getFærdigeBatches());
+        this.add(cmbBatches,0,1);
+
+
+
+
+
+    }
 }
