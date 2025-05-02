e62f9a5e98b511e8e7c63023a79599c40cde99f5
Katrine <katrine28@hotmail.dk>
2025-05-02 12:29:41 +0200

    start på Destillat

--- src/application/model/Destillat.java
+++ src/application/model/Destillat.java
@@ -1,4 +1,46 @@
 package application.model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Destillat {
+    private static int idTæller = 1;
+    private String navn;
+    private int id;
+    private double alkoholprocent;
+    private LocalDate påfyldningsDato;
+
+    private Fad fad;
+    private final List<Mængde> mængder = new ArrayList<>();
+
+    public Destillat(String navn, Fad fad) {
+        this.navn = navn;
+        this.fad = fad;
+        id = idTæller;
+        idTæller++;
+    }
+
+    public void setAlkoholprocent(double alkoholprocent) {
+        this.alkoholprocent = alkoholprocent;
+    }
+
+    public void setPåfyldningsDato(LocalDate påfyldningsDato) {
+        this.påfyldningsDato = påfyldningsDato;
+    }
+
+    //sammenhæng til mængde
+
+    public List<Mængde> getMængder() {
+        return new ArrayList<>(mængder);
+    }
+
+    public Mængde createMængde (double antalLiter, Batch batch){
+        Mængde mængde
+    }
+
+
+    //metoder
+
+
 }
