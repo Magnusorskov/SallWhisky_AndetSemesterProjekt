c125e13ff16d5e15852a6c7520aecd4a7221fb5a
Magnus Orskov <magnusorskov@hotmail.com>
2025-05-07 12:14:40 +0200

    batch test

--- test/application/model/BatchTest.java
+++ test/application/model/BatchTest.java
@@ -14,8 +14,9 @@ class BatchTest {
     @BeforeEach
     void setUp() {
         batch = new Batch(Bygsort.EVERGREEN, Mark.MOSEVANG, "CLN", "Tørv", "Nr11", LocalDate.of(2025, 01, 01), "Test start");
-
+        batch.setVæskemængde(10);
     }
+
 
     @Test
     void test01_KontruktorBatch() {
@@ -27,21 +28,42 @@ class BatchTest {
         assertEquals("Nr11", batch.getMaltBatch(), "Maltbatch er forkert");
         assertEquals(LocalDate.of(2025, 01, 01), batch.getStartDato(), "Startdatoen er forkert");
         assertEquals("Test start", batch.getKommentar(), "Kommentar er forkert");
-        assertEquals(1, batch.getId());
-
     }
 
     @Test
     void test02_KonstruktorBatch() {
         batch = new Batch(Bygsort.EVERGREEN, Mark.MOSEVANG, "CLN", null, "Nr11", LocalDate.of(2025, 01, 01), null);
         assertNotNull(batch, "Batch ikke oprettet");
-        assertEquals(Bygsort.EVERGREEN, batch.getBygsort(),"Bygsort er forkert");
-        assertEquals(Mark.MOSEVANG, batch.getMark(),"Marken er forkert");
-        assertEquals("CLN",batch.getInitialer(), "Initialer er forkert");
-        assertEquals("Nr11",batch.getMaltBatch(),"Maltbatch er forkert");
-        assertEquals(LocalDate.of(2025,01,01), batch.getStartDato(), "Startdatoen er forkert");
+        assertEquals(Bygsort.EVERGREEN, batch.getBygsort(), "Bygsort er forkert");
+        assertEquals(Mark.MOSEVANG, batch.getMark(), "Marken er forkert");
+        assertEquals("CLN", batch.getInitialer(), "Initialer er forkert");
+        assertEquals("Nr11", batch.getMaltBatch(), "Maltbatch er forkert");
+        assertEquals(LocalDate.of(2025, 01, 01), batch.getStartDato(), "Startdatoen er forkert");
         assertNull(batch.getKommentar(), "Kommentar ikke null");
         assertNull(batch.getRygemateriale(), "Rygemateriale ikke null");
-        assertEquals(1,batch.getId());
+    }
+
+    @Test
+    void test01_tapBatch() {
+        batch.tapBatch(1);
+        assertEquals(9,batch.getVæskemængde());
+    }
+
+    @Test
+    void test02_tapBatch() {
+        batch.tapBatch(5);
+        assertEquals(5,batch.getVæskemængde());
+    }
+
+    @Test
+    void test03_tapBatch() {
+        batch.tapBatch(9);
+        assertEquals(1,batch.getVæskemængde());
+    }
+
+    @Test
+    void test04_tapBatch() {
+        batch.tapBatch(10);
+        assertEquals(0,batch.getVæskemængde());
     }
 }
