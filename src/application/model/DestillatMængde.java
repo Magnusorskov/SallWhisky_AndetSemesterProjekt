d13805ae71e70937fc5846d7725d5e75b311db48
Magnus Orskov <magnusorskov@hotmail.com>
2025-05-08 12:37:48 +0200

    Dokumentation på DestillatMængde.

--- src/application/model/DestillatMængde.java
+++ src/application/model/DestillatMængde.java
@@ -6,17 +6,34 @@ public class DestillatMængde implements Serializable {
     private double antalLiter;
     private Destillat destillat;
 
-    // TODO dokumentation
+    /**
+     * Initialiserer en DestillatMængdes antal liter og destillat.
+     * Pre: antalLiter er større end 0.
+     * Pre: destillat er ikke null.
+     *
+     * @param antalLiter det antal liter man tapper fra batchen.
+     * @param destillat  det destillat man tager væske fra.
+     */
     DestillatMængde(double antalLiter, Destillat destillat) {
         this.antalLiter = antalLiter;
         this.destillat = destillat;
         destillat.tapDestillat(antalLiter);
     }
 
+    /**
+     * Returnerer en double med DestillatMængdens liter.
+     *
+     * @return DestillatMængdens liter som double.
+     */
     public double getAntalLiter() {
         return antalLiter;
     }
 
+    /**
+     * Returnerer DestillatMængdens tilknyttede batch.
+     *
+     * @return DestillatMængdens destillat.
+     */
     public Destillat getDestillat() {
         return destillat;
     }
