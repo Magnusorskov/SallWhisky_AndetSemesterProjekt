7a2d9b48b0dfd06d3fe60f92cd4bd1fe948b901a
Magnus Orskov <magnusorskov@hotmail.com>
2025-05-08 13:10:11 +0200

    Dokumentation for Storage interface.

--- src/application/controller/Storage.java
+++ src/application/controller/Storage.java
@@ -5,38 +5,127 @@ import application.model.*;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Interface der definerer de metoder, som en klasse der håndterer lagerstyring skal implementere.
+ * Dette inkluderer metoder til at hente, tilføje og fjerne forskellige typer af lagrede enheder.
+ */
 public interface Storage {
+    /**
+     * Henter en liste over de batches der er igangværende.
+     *
+     * @return en liste af Batch objekter som er igangværende.
+     */
     public List<Batch> getIgangværendeBatches();
 
+    /**
+     * Henter en liste over alle fade i systemet.
+     *
+     * @return en liste af Fad objekter.
+     */
     public List<Fad> getFade();
 
+    /**
+     * Henter en liste over de batches der er færdige.
+     *
+     * @return en liste af Batch objekter som er færdige.
+     */
     public List<Batch> getFærdigeBatches();
 
+    /**
+     * Henter en liste over alle lagre i systemet.
+     *
+     * @return en liste af Lager objekter.
+     */
     public List<Lager> getLagre();
 
+    /**
+     * Henter en liste over alle destillater i systemet.
+     *
+     * @return en liste af Destillat objekter.
+     */
     public List<Destillat> getDestillater();
 
+    /**
+     * Henter en mængde af alle whiskyer i systemet.
+     *
+     * @return et Set af unikke Whisky objekter.
+     */
     public Set<Whisky> getWhiskyer();
 
+    /**
+     * Tilføjer en ny batch til systemet.
+     *
+     * @param batch det Batch objekt der skal tilføjes.
+     */
     public void addBatch(Batch batch);
 
+    /**
+     * Tilføjer et nyt fad til systemet.
+     *
+     * @param fad det Fad objekt der skal tilføjes.
+     */
     public void addFad(Fad fad);
 
+    /**
+     * Fjerner en igangværende batch.
+     *
+     * @param batch det Batch objekt der skal fjernes fra de igangværende.
+     */
     public void removeIgangværendeBatch(Batch batch);
 
+    /**
+     * Tilføjer en færdig batch.
+     *
+     * @param batch det Batch objekt der skal tilføjes.
+     */
     public void addFærdigBatch(Batch batch);
 
+    /**
+     * Tilføjer et nyt lager til systemet.
+     *
+     * @param lager det Lager objekt der skal tilføjes.
+     */
     public void addLager(Lager lager);
 
+    /**
+     * Tilføjer et nyt destillat til systemet.
+     *
+     * @param destillat det Destillat objekt der skal tilføjes.
+     */
     public void addDestillat(Destillat destillat);
 
+    /**
+     * Tilføjer en ny whisky til systemet.
+     *
+     * @param whisky det Whisky objekt der skal tilføjes.
+     */
     public void addWhisky(Whisky whisky);
 
+    /**
+     * Genererer et nyt unikt ID for en whisky.
+     *
+     * @return et nyt unikt ID til en whisky.
+     */
     public int whiskyID();
 
+    /**
+     * Genererer et nyt unikt ID for en batch.
+     *
+     * @return et nyt unikt ID til en batch.
+     */
     public int batchID();
 
+    /**
+     * Genererer et nyt unikt ID for et fad.
+     *
+     * @return et nyt unikt ID til et fad.
+     */
     public int fadID();
 
+    /**
+     * Genererer et nyt unikt ID for et destillat.
+     *
+     * @return et nyt unikt ID til et destillat.
+     */
     public int destillatID();
 }
--- src/storage/ListStorage.java
+++ src/storage/ListStorage.java
@@ -19,29 +19,29 @@ public class ListStorage implements Storage, Serializable {
     private final Set<Whisky> whiskyer = new TreeSet<>();
     private final int[] ids = new int[4];
 
-    // TODO dokumentation
+    
     @Override
     public List<Batch> getIgangværendeBatches() {
         return new ArrayList<>(igangværendeBatches);
     }
-    // TODO dokumentation
+    
     public List<Batch> getFærdigeBatches() {
         return new ArrayList<>(færdigeBatches);
     }
-    // TODO dokumentation
+    
     @Override
     public void addBatch(Batch batch) {
         if (!igangværendeBatches.contains(batch)) {
             igangværendeBatches.add(batch);
         }
     }
-    // TODO dokumentation
+    
     public void removeIgangværendeBatch(Batch batch) {
         if (igangværendeBatches.contains(batch)) {
             igangværendeBatches.remove(batch);
         }
     }
-    // TODO dokumentation
+    
     public void addFærdigBatch(Batch batch) {
         if (!færdigeBatches.contains(batch)) {
             færdigeBatches.add(batch);
@@ -49,12 +49,12 @@ public class ListStorage implements Storage, Serializable {
     }
 
     // -------------------------------------------------------------------------
-    // TODO dokumentation
+    
     @Override
     public List<Fad> getFade() {
         return new ArrayList<>(fade);
     }
-    // TODO dokumentation
+    
     @Override
     public void addFad(Fad fad) {
         if (!fade.contains(fad)) {
@@ -63,12 +63,12 @@ public class ListStorage implements Storage, Serializable {
     }
 
     //--------------------------------------------------------
-    // TODO dokumentation
+    
     @Override
     public List<Lager> getLagre() {
         return new ArrayList<>(lagre);
     }
-    // TODO dokumentation
+    
     @Override
     public void addLager(Lager lager) {
         if (!lagre.contains(lager)) {
@@ -77,37 +77,37 @@ public class ListStorage implements Storage, Serializable {
     }
 
     //------------------------------------------------------
-    // TODO dokumentation
+    
     @Override
     public List<Destillat> getDestillater() {
         return new ArrayList<>(destillater);
     }
-    // TODO dokumentation
+    
     @Override
     public void addDestillat(Destillat destillat) {
         if (!destillater.contains(destillat)) {
             destillater.add(destillat);
         }
     }
-    // TODO dokumentation
+    
     public int whiskyID() {
         ids[0]++;
         int id = ids[0];
         return id;
     }
-    // TODO dokumentation
+    
     public int batchID() {
         ids[1]++;
         int id = ids[1];
         return id;
     }
-    // TODO dokumentation
+    
     public int fadID() {
         ids[2]++;
         int id = ids[2];
         return id;
     }
-    // TODO dokumentation
+    
     public int destillatID() {
         ids[3]++;
         int id = ids[3];
@@ -115,12 +115,12 @@ public class ListStorage implements Storage, Serializable {
     }
 
     //------------------------------------------------------
-    // TODO dokumentation
+    
     @Override
     public Set<Whisky> getWhiskyer() {
         return new TreeSet<>(whiskyer);
     }
-    // TODO dokumentation
+    
     @Override
     public void addWhisky(Whisky whisky) {
         whiskyer.add(whisky);
